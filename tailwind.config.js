469b75c1e8d27d37afd39cb200792324d25ef70e
0kohout <0kohout@ziegler.de>
2020-12-14 13:43:42 +0100

    finish

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,5 +7,9 @@ module.exports = {
   variants: {
     extend: {},
   },
-  plugins: [require("@tailwindcss/typography"), require("@tailwindcss/forms")],
+  plugins: [
+    require("@tailwindcss/typography"),
+    require("@tailwindcss/forms"),
+    require("@tailwindcss/aspect-ratio"),
+  ],
 };
