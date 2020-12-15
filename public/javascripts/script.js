0185abf377f198d9a54102c096242a3edf83ada7
envalde <kohoutdanie@gmail.com>
2020-12-15 06:44:57 +0100

    JS-Funktionen

---
+++ public/javascripts/script.js
@@ -0,0 +1,1 @@
+function close_notice(){var e,s,i=document.getElementById("update-notice");i.classList?i.classList.toggle("hidden"):(0<=(s=(e=i.className.split(" ")).indexOf("hidden"))?e.splice(s,1):e.push("hidden"),i.className=e.join(" "))}
