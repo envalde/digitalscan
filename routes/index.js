11ec7e41c215bb171836d7c50400f240f881296a
envalde <kohoutdanie@gmail.com>
2020-12-15 07:28:14 +0100

    titel

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Scan-Card - Digitaler Vertrieb für mehr Kunden und Umsatz' });
 });
 
 module.exports = router;
